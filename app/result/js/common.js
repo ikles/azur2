9b156c2bc447482bda83b3612ffa2ecfea6830e1
Igor <membship@ya.ru>
2021-12-11 20:33:22 +0700

    first commit

---
+++ app/result/js/common.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function( $ ) {
+
+  $(".toggle-mnu").click(function() {
+    $(this).toggleClass("on");
+    $(".top-mnu").slideToggle();
+    return false;
+  });
+
+  $('body').click(function () {
+    if( $(".toggle-mnu").hasClass("on") ){
+      $(".toggle-mnu").removeClass("on");
+      $(".top-mnu").fadeOut();
+    }
+  });
+
+
+  $(".top-mnu").click(function (e) {
+    e.stopPropagation();
+  });
+
+
+  /************************************/
+
+/*$('.wrapper').prepend('<span class="eye-3"></span>');
+let pg = parseInt(document.location.pathname.match(/\d+/))
+$('body').addClass('active').css('background-image', "url('../img/"+pg+".jpg')");
+$('body:not(.active)').css('background-image', "unset");
+
+$('.eye-3').click(function (e) {
+  e.preventDefault();  
+  $('body').toggleClass('active');
+  let pg = parseInt(document.location.pathname.match(/\d+/));
+  $('body.active').css('background-image', "url('../img/"+pg+".jpg')");
+  $('body:not(.active)').css('background-image', "unset");
+
+});*/
+
+/************************************/
+
+$('.signup-close').click(function () {
+  $('.signup').fadeOut();
+});
+
+}); //ready
+
